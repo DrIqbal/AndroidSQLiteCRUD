c3229085470dfbda380eee82fb318415c30f417d
Dr.Iqbal <aiqbal@tmsk.uitm.edu.my>
2017-10-20 15:55:41 +0800

    Initial commit

---
+++ app/src/main/java/com/example/iqbal/androidsqlitecrud/ViewBiodataActivity.java
@@ -0,0 +1,13 @@
+package com.example.iqbal.androidsqlitecrud;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViewBiodataActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_biodata);
+    }
+}
